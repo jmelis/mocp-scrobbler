f21e6cbbf5a12e4ca07f688ff1ff9913b0b5d12e
Jaime Melis <j.melis@fdi.ucm.es>
2009-11-10 20:07:51 +0100

    restructuring the program

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require 'rubygems'
 require 'scrobbler'
+require 'pp'
 
 # http://www.last.fm/api/submissions
 # http://scrobbler.rubyforge.org/docs/
@@ -16,25 +17,44 @@ AUTHFILE='authfile'
 POLL_INTERVAL=1
 
 # debug (don't do the actual scrobbling)
-DEBUG = true
+# DEBUG = true
+
+# log to file
+# OUTPUT = 'mocp-scrobbler.log'
 #############################
+
 
 class MocpScrobbler
     attr_reader :fileinfo
 
     def initialize
         connect if !defined? DEBUG
+        @logfile = File.open(OUTPUT,'a') if defined? OUTPUT
+    end
+
+    def log(str)
+        if defined? OUTPUT
+            @logfile.write(str+"\n")
+        else
+            puts str
+        end
     end
 
     def connect
-        (login,password) = File.new(AUTHFILE).readlines[0].chomp.split(':')
-        @auth = Scrobbler::SimpleAuth.new(:user => login, :password => password)
-        @auth.handshake!
+        #(login,password) = File.new(AUTHFILE).readlines[0].chomp.split(':')
+        #@auth = Scrobbler::SimpleAuth.new(:user => login, :password => password)
+        #@auth.handshake!
     end
 
     def get_fileinfo
         fileinfo = Hash.new
-        `mocp -i`.split("\n").each{|l| (k,v) = l.split(': '); fileinfo[k] = v}
+        `mocp -i`.split("\n").each do |l| 
+            (k,v) = l.split(': ')
+            if v.class != String
+                v = String.new
+            end
+            fileinfo[k] = v.strip
+        end
         fileinfo
     end
 
@@ -46,25 +66,24 @@ class MocpScrobbler
             if !@fileinfo
                 # now playing submission
                 @fileinfo = fileinfo_poll
-                puts "now playing: " + @fileinfo['SongTitle']
+                log "now playing: " + @fileinfo['SongTitle']
                 @time_start = Time.now
                 submit_now_playing if !defined? DEBUG
             else
                 if changed(fileinfo_poll)
                     if submit_allowed
                         submit_scrobble if !defined? DEBUG
-                        puts "scrobble: " + @fileinfo['SongTitle']
+                        log "scrobble: " + @fileinfo['SongTitle']
                     end
                     @time_start = Time.now
                     @fileinfo = fileinfo_poll
-                    puts "now playing: " + @fileinfo['SongTitle']
                     submit_now_playing if !defined? DEBUG
                 else
                     @fileinfo = fileinfo_poll
                 end
             end
         else
-            puts "server status STOP"
+            log "server status STOP"
         end
     end
 
@@ -74,46 +93,55 @@ class MocpScrobbler
     end
 
     def submit_allowed
-        totalsec = @fileinfo['TotalSec'].to_i
-        currentsec = @fileinfo['CurrentSec'].to_i
-        halfsec = totalsec/2
+ #{totalsec.to_s}, CurrentSec: #{currentsec.to_s}, HalfSec: #{(totalsec/2).to_s}"
 
-        puts "TotalSec: #{totalsec.to_s}, CurrentSec: #{currentsec.to_s}, HalfSec: #{halfsec.to_s}"
         if totalsec <= 30
-            puts "song shorter that 30s"
-            puts "no scrobbling allowed"
+            log "song shorter that 30s"
+            log "no scrobbling allowed"
             return false
         end
+
         if currentsec >= 240 or currentsec >= totalsec/2
             return true
         else
-            puts "no scrobbling allowed"
+            log "no scrobbling allowed"
             return false
         end
     end
 
-    def submit_now_playing
-        playing = Scrobbler::Playing.new(:session_id => @auth.session_id,
-                                     :now_playing_url => @auth.now_playing_url,
-                                     :artist        => @fileinfo['Artist'],
-                                     :track         => @fileinfo['SongTitle'],
-                                     :album         => @fileinfo['Album'],
-                                     :length        => @fileinfo['TotalSec'],
-                                     :track_number  => '')
+    def has_tags
+        fields = %w(Artist SongTitle Album)
+        fields.each {|f| return false if @fileinfo[f].empty? }
+        true
+    end
 
-        playing.submit!
+    def submit_now_playing
+        if !has_tags
+            log "#{fileinfo['File']} has no tags"
+        else
+            #playing = Scrobbler::Playing.new(:session_id => @auth.session_id,
+            #                             :now_playing_url => @auth.now_playing_url,
+            #                             :artist        => @fileinfo['Artist'],
+            #                             :track         => @fileinfo['SongTitle'],
+            #                             :album         => @fileinfo['Album'],
+            #                             :length        => @fileinfo['TotalSec'],
+            #                             :track_number  => '')
+
+            #playing.submit!
+            log "now playing: " + @fileinfo['SongTitle']
+        end
     end
 
     def submit_scrobble
-        scrobble = Scrobbler::Scrobble.new(:session_id => @auth.session_id,
-                                       :submission_url => @auth.submission_url,
-                                       :artist        => @fileinfo['Artist'],
-                                       :track         => @fileinfo['SongTitle'],
-                                       :album         => @fileinfo['Album'],
-                                       :time          => @time_start,
-                                       :length        => @fileinfo['TotalSec'],
-                                       :track_number => '')
-        scrobble.submit!
+        #scrobble = Scrobbler::Scrobble.new(:session_id => @auth.session_id,
+        #                               :submission_url => @auth.submission_url,
+        #                               :artist        => @fileinfo['Artist'],
+        #                               :track         => @fileinfo['SongTitle'],
+        #                               :album         => @fileinfo['Album'],
+        #                               :time          => @time_start,
+        #                               :length        => @fileinfo['TotalSec'],
+        #                               :track_number => '')
+        #scrobble.submit!
     end
 end
 
